ebaed2d97088e08f20475d2b6dc2242c710d43cb
baselamr14 <bassel.odoo18@gmail.com>
2026-02-02 13:02:18 +0200

    Delete asset-management-production directory

--- asset-management-production/ivs_asset_management/models/account_asset_prorata_fix.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class AccountAsset(models.Model):
-    _inherit = "account.asset"
-
-    # remove ORM required constraint (server-side)
-    prorata_date = fields.Date(required=False)
