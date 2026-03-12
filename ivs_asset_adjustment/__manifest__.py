7c6a063f9bfe96c2ee7d54610d6d26588e8f59d4
baselamr14 <bassel.odoo18@gmail.com>
2026-03-12 13:56:57 +0200

    Add files via upload

--- ivs_asset_adjustment/__manifest__.py
+++ ivs_asset_adjustment/__manifest__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 {
     'name': 'Asset Adjustment',
     'version': '19.0.1.0.0',
@@ -20,7 +18,7 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'asset_adjustment/static/src/scss/statusbar.scss',
+            'ivs_asset_adjustment/static/src/scss/statusbar.scss',
         ],
     },
     'installable': True,
@@ -28,12 +26,11 @@
     'application': True,
     'license': 'AGPL-3',
 }
-
 # # -*- coding: utf-8 -*-
 #
 # {
 #     'name': 'Asset Adjustment',
-#     'version': '16.0.1',
+#     'version': '19.0.1.0.0',
 #     'category': 'Accounting',
 #     'summary': """
 #        Asset Adjustment
@@ -49,8 +46,39 @@
 #         'reports/adjustment_report.xml',
 #         'wizards/asset_adjustment_wizard.xml',
 #     ],
+#     'assets': {
+#         'web.assets_backend': [
+#             'asset_adjustment/static/src/scss/statusbar.scss',
+#         ],
+#     },
 #     'installable': True,
 #     'auto_install': False,
 #     'application': True,
 #     'license': 'AGPL-3',
 # }
+#
+# # # -*- coding: utf-8 -*-
+# #
+# # {
+# #     'name': 'Asset Adjustment',
+# #     'version': '16.0.1',
+# #     'category': 'Accounting',
+# #     'summary': """
+# #        Asset Adjustment
+# #     """,
+# #     'description': """This Module Manage Asset Adjustment""",
+# #     'author': 'IValue Solutions Team',
+# #     'depends': ['ivs_asset_management'],
+# #     'data': [
+# #         'security/security.xml',
+# #         'security/ir.model.access.csv',
+# #         'data/ir_sequence.xml',
+# #         'views/asset_adjustment.xml',
+# #         'reports/adjustment_report.xml',
+# #         'wizards/asset_adjustment_wizard.xml',
+# #     ],
+# #     'installable': True,
+# #     'auto_install': False,
+# #     'application': True,
+# #     'license': 'AGPL-3',
+# # }
